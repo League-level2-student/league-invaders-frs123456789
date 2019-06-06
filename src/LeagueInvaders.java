e12ecef621019675d6a94756492ad01c5311e98f
League Student <league@wtsim17.attlocal.net>
2019-06-05 19:57:27 -0700

    Meh

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,6 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+JFrame jf;
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+	
+public LeagueInvaders() {
+	jf = new JFrame();
+	jf.setVisible(true);
+	jf.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+}
+void setup() {
+	int height = HEIGHT;
+	int width = WIDTH;
+
+}
 public static void main(String[] args) {
-	//I did nothing!
+LeagueInvaders li = new LeagueInvaders();
+li.setup();
+
 }
 }
+           
