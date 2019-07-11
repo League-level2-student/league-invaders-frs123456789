6c389aff91e893abb347ce02bf567cb939d8fb70
League Student <league@iMac-22.attlocal.net>
2019-07-10 19:56:46 -0700

    GameLoop, not working

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,54 +1,88 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
 	final int MENU = 0;
-    final int GAME = 1;
-    final int END = 2;
-    Font titleFont;
-    int currentState = MENU;
+	final int GAME = 1;
+	final int END = 2;
+	Font titleFont;
+	Font instuctionFont;
+	int currentState = MENU;
+	Timer frameDraw;
+	GamePanel() {
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+		instuctionFont = new Font("Arial", Font.PLAIN, 35);
+	    frameDraw = new Timer(1000/60,this);
+	    frameDraw.start();
 
-    GamePanel(){
-        titleFont = new Font("Arial", Font.PLAIN, 48);
-    }
-    
+	}
+
 	@Override
-	public void paintComponent(Graphics g){
-		if(currentState == MENU){
-		    drawMenuState(g);
-		}else if(currentState == GAME){
-		    drawGameState(g);
-		}else if(currentState == END){
-		    drawEndState(g);
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
 		}
 	}
-	void updateMenuState() {  
+
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+	}
+
+	void updateEndState() {
+		
 		
 	}
-	 void updateGameState() {  
-		 
-	 }
-	void updateEndState()  {  
-		
-	}
-	void drawMenuState(Graphics g) { 
+
+	void drawMenuState(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		g.setFont(titleFont);
 		g.setColor(Color.YELLOW);
-		g.drawString("text", 200,400);
+		g.drawString("Press ENTER to start!", 15, 400);
+		g.drawString("League Invaders!", 55, 100);
+		g.setFont(instuctionFont);
+		g.drawString("Press SPACE for instructions", 15, 600);
 	}
-	void drawGameState(Graphics g) {  
+
+	void drawGameState(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-	 }
-	 void drawEndState(Graphics g)  {  
-		 g.setColor(Color.RED);
-			g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-	 }
-	
-	
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("GAME OVER :(", 15, 150);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		    System.out.println("action");
+		    repaint();
+		}
+	}
+
 }
